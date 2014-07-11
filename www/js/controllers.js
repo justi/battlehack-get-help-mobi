ce5b3d26dc1f4f6832aecfd63e6b50bcc1ec4f51
Krzysztof Hasiński <krzysztof.hasinski@gmail.com>
2014-07-11 17:06:27 +0200

    autolocate on map start

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,60 +15,40 @@ angular.module('starter.controllers', [])
 })
 
 .controller('MapCtrl', function($scope, $ionicLoading, $compile) {
-  function initialize() {
-    var myLatlng = new google.maps.LatLng(43.07493,-89.381388);
 
-    var mapOptions = {
-      center: myLatlng,
-      zoom: 16,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map"),
-        mapOptions);
-
-    //Marker + infowindow + angularjs compiled ng-click
-    var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
-    var compiled = $compile(contentString)($scope);
-
-    var infowindow = new google.maps.InfoWindow({
-      content: compiled[0]
-    });
-
-    var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      title: 'Uluru (Ayers Rock)'
-    });
-
-    google.maps.event.addListener(marker, 'click', function() {
-      infowindow.open(map,marker);
-    });
-
-    $scope.map = map;
-  }
-  initialize();
-  google.maps.event.addDomListener(window, 'load', initialize);
-
-  $scope.centerOnMe = function() {
-    if(!$scope.map) {
-      return;
-    }
 
     $scope.loading = $ionicLoading.show({
-      content: 'Getting current location...',
-      showBackdrop: false
+        content: 'Getting current location&hellip;',
+        showBackdrop: false
     });
 
+  $scope.centerOnMe = function() {
     navigator.geolocation.getCurrentPosition(function(pos) {
+        var myLatlng = new google.maps.LatLng(43.07493,-89.381388);
+
+        var mapOptions = {
+            center: myLatlng,
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map"),
+            mapOptions);
+
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            title: 'Uluru (Ayers Rock)'
+        });
+
+        $scope.map = map;
       $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-      $scope.loading.hide();
+        $scope.loading.hide();
     }, function(error) {
       alert('Unable to get location: ' + error.message);
+        $scope.loading.hide();
     });
   };
 
-  $scope.clickTest = function() {
-    alert('Example of infowindow with ng-click')
-  };
+  $scope.centerOnMe();
 
 });
